7d5e719b601c19eb99966b77fc6275bf1107f9da
Vladislav <vladislav.v.o@yandex.ru>
2022-09-27 00:10:10 +0300

    3d task done

--- task3.py
+++ task3.py
@@ -3,3 +3,26 @@
 # Если перевернутое число не равно исходному, то они складываются и проверяются на палиндром еще раз.
 # Это происходит до тех пор, пока не будет найден палиндром.
 # Напишите такую программу, которая найдет палиндром введенного пользователем числа
+
+def check_input_number(inputed):
+    is_correct = False
+    while not is_correct:
+        try:
+            number = int(input(f"{inputed}"))
+            is_correct = True
+        except ValueError:
+            print("Please input number")
+    return number
+
+def find_palindrom (number):
+    number = str(number)
+    while True:
+        number2 = "".join(reversed(number))
+        if number2 == number:
+            print("Poly is: ", number2)
+            break
+        else:
+            number = str(int(number) + int(number2))
+
+number = check_input_number('Input number: ')
+find_palindrom (number)
