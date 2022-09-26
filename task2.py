73be2b87aa3a3813c116b37af329392560ed959d
Vladislav <vladislav.v.o@yandex.ru>
2022-09-26 22:48:34 +0300

    2nd task done

--- task2.py
+++ task2.py
@@ -4,3 +4,25 @@
 
 # Пример:
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def check_input_number(inputed):
+    is_correct = False
+    while not is_correct:
+        try:
+            number = int(input(f"{inputed}"))
+            is_correct = True
+        except ValueError:
+            print("Please input number")
+    return number
+
+def add_in_list(n):
+    list = []
+    number = 1
+    for i in range (1, n+1):
+        number *= i
+        list.append(number)
+    print(f"Произведение чисел от 1 до {n}: {list}")
+
+n = check_input_number('Input number: ')
+number = n
+add_in_list(number)
